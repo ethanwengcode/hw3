85843d6597dadb0ab4bfeb74fb1be1c9701947a5
Ethan Weng <ethanwen@usc.edu>
2025-03-07 09:30:46 +0000

    fixes to llrec

--- llrec.cpp
+++ llrec.cpp
@@ -5,23 +5,24 @@
 //*********************************************
 void llpivot (Node*& head, Node*& smaller, Node*& larger, int pivot) {
 
-  if( head == NULL ){
+  if( head == NULL ){ 
+    smaller = NULL;  // end the linked lists
+    larger = NULL;
     return;
   }
 
   Node* temp = head->next; 
 
-  if( head->val > pivot ){
-    head->next = larger;
+  if( head->val > pivot ){ // current value is larger than pivot
     larger = head;
+    llpivot(temp, smaller, larger->next, pivot);
   }
-  else if( head->val <= pivot ){
-    head->next = smaller;
+  else if( head->val <= pivot ){ // current value is smaller than or equal to pivot 
     smaller = head;
+    llpivot(temp, smaller->next, larger, pivot);
   }  
 
   head = NULL;
-  llpivot(temp, smaller, larger, pivot);
 
 }
 
